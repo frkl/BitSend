281393e1f7f639b267c49a0fdf61007252d94d5d
Xiao Lin <linxiao.frank@gmail.com>
2018-04-06 22:18:00 -0400

    fix extra nonce

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -382,10 +382,13 @@ UniValue getwork(const JSONRPCRequest& request)
         CBlock* pblock = mapNewBlock[pdata->hashMerkleRoot].first;
         pblock->nTime = pdata->nTime;
         pblock->nNonce = pdata->nNonce;
+		CMutableTransaction txCoinbase(*pblock->vtx[0]);
+		txCoinbase.vin[0].scriptSig = mapNewBlock[pdata->hashMerkleRoot].second;
+		pblock->vtx[0] = MakeTransactionRef(std::move(txCoinbase));
+		pblock->hashMerkleRoot = BlockMerkleRoot(*pblock);
         //CMutableTransaction txCoinbase(*pblock->vtx[0]);
         //txCoinbase.vin[0].scriptSig = mapNewBlock[pdata->hashMerkleRoot].second;
 		//pblock->vtx[0] = MakeTransactionRef(std::move(txCoinbase));
-        pblock->hashMerkleRoot = BlockMerkleRoot(*pblock);
         assert(pwalletMain != NULL);
         return CheckWork(pblock, *pwalletMain, *pMiningKey);
     }
